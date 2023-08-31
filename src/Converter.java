8f1d84da2286d33f07c82c111dba55678a5feb6b
ElizabethRudova <rudova_l@list.ru>
2023-08-31 12:03:17 +0700

    пробую выкладывать правильно

--- src/Converter.java
+++ src/Converter.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class Converter {
+import java.util.TreeMap;
+
+ class Converter {
+    TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
+    TreeMap<Integer, String> arabianKeyMap = new TreeMap<>();
+
+    public Converter(){
+        romanKeyMap.put('I', 1); //ключи заглавных символов для ввода с консоли.
+        romanKeyMap.put('V', 5);
+        romanKeyMap.put('X', 10);
+        romanKeyMap.put('L', 50);
+        romanKeyMap.put('C', 100);
+        romanKeyMap.put('D', 500);
+        romanKeyMap.put('M', 1000);
+
+
+        arabianKeyMap.put(1000, "M");
+        arabianKeyMap.put(900, "CM");
+        arabianKeyMap.put(500, "D");
+        arabianKeyMap.put(400, "CD");
+        arabianKeyMap.put(100, "C");
+        arabianKeyMap.put(90, "XC");
+        arabianKeyMap.put(50, "L");
+        arabianKeyMap.put(40, "XL");
+        arabianKeyMap.put(10, "X");
+        arabianKeyMap.put(9, "IX");
+        arabianKeyMap.put(5, "V");
+        arabianKeyMap.put(4, "IV");
+        arabianKeyMap.put(1, "I");
+
+
+    }
+
+    public boolean isRoman (String number){         //  метод принимает строку и сверяет ее с данными ключами метода Converter.
+        return romanKeyMap.containsKey(number.charAt(0)); //конвертация строкового значения в тип char, сверка полученного результата с существующими ключами.
+    }
+
+    public int romanToArabian(String s)
+    {
+        int end = s.length()-1; //задаем конечный индекс.
+
+        char[] charArray = s.toCharArray(); //конвертируем строковый массив в символьный.
+
+        int arabian;
+        int result = romanKeyMap.get(charArray[end]); // сразу конвертируем последний символ.
+
+        for(int i = end-1; i>=0; i--) //цикл начнет свой ход с предпоследнего значения в массиве.
+        {
+            arabian=romanKeyMap.get(charArray[i]);
+
+            if(arabian<romanKeyMap.get(charArray[i+1])) //сравнение двух соседнестоящих с конца символов.
+            {
+                result-=arabian;
+            } else {
+                result+=arabian;
+            }
+
+        }
+
+        return result;
+    }
+
+    public String arabianToRoman( int number){ //метод конвертирует арабские числа в римские.
+      String romanNumber = "";
+      int arabianKey;
+      do
+        {
+          arabianKey = arabianKeyMap.floorKey(number); //floorKey ищет ближейшее нужное значение снизу вверх.
+          romanNumber+=arabianKeyMap.get(arabianKey); //берется подходящий ключ из списка и записывается в строку
+          number -= arabianKey; //из нашего исходного числа отнимается значение уже записанного ключа.
+        } while (number != 0); //цикл работает пока выполняются условия.
+
+      return  romanNumber;
+    }
+
+
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,121 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    static Scanner inputNumber= new Scanner(System.in); // ввод данных с клавиатуры.
+    static String inputLine = inputNumber.next(); // заполнение текстовой ячейки данными со сканера.
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws IOException {
+
+
+        arabicNumerals(); //вызов метода с основным кодом вычисления.
+
+    }
+
+
+    public static int arabicNumerals  () throws IOException {
+        Converter converter = new Converter();
+
+
+        char operation = getOperation(); // используем метод для определения знака действия.
+
+
+            String[] strings = inputLine.split("\\W"); /*строковое деление через регулярное выражение, поиск любого символа, кроме латиницы,цифр и нижнего
+         подчеркивания.*/
+            if (strings.length<2 || strings.length>2){  //исключение! Формат математической операции не удовлетворяет заданию-два операнда и 1 оператор.
+                throw new IllegalArgumentException("Формат математической операции не удовлетворяет заданию-два целочисленных операнда и 1 оператор.");
+
+         }
+
+
+        if(converter.isRoman(strings[0])== converter.isRoman(strings[1])) { //проверка на совпадение формата введенных чисел.
+
+            int a;
+            int b;
+
+            boolean romanNumber = converter.isRoman(strings[0]); //ввод логической переменной для обнаружения римского числа.
+
+
+            if(romanNumber){ //если условие true, то идет конвертация римского числа в арабское.
+
+
+                    a = converter.romanToArabian(strings[0]);
+                    b = converter.romanToArabian(strings[1]);
+
+                        if(mathematicalCalculation(a,b,operation)<=0){
+                            throw new IOException("Результат вычисления римских чисел должен быть больше нуля!"); //исключение! результат вычисления римских чисел строго >0.
+                        }
+
+
+
+                    if (a <= 10 && b <= 10) {
+                        System.out.println("Результат = " + converter.arabianToRoman(mathematicalCalculation(a, b, operation))); /* вывод полученного после расчета числа, обратно
+                 сконвертированного из арабского в римское.*/
+                    } else {
+
+                            throw new IOException("Введены числа больше значения 10!"); //исключение! Разрешен ввод чисел не больше числа 10.
+
+                    }
+
+            }else{
+
+                        a = Integer.parseInt(strings[0]); //перевод строкового значения в числовое.
+                        b = Integer.parseInt(strings[1]);
+
+                    if(a<=10 && b<=10){
+                        System.out.println("Результат = " + mathematicalCalculation(a, b, operation)); //вывод математического расчета из отдельного метода.
+                    }else{
+                        throw new IOException("Введены числа больше значения 10"); //исключение! Разрешен ввод чисел не больше числа 10.
+                        }
+                    }
+            }
+
+        else
+        {
+            throw new NumberFormatException("Числа введены в разных форматах.");
+
+        }
+
+        return 0;
+    }
+    public static char getOperation () throws IOException //метод позволяет определить знак действия в приведенном примере.
+    {
+        char[] symbol = inputLine.toCharArray(); //переводим строковый массив в символьный.
+        char symbolOperation = 0;
+        for(int i = 0; i<symbol.length; i++){ // цикл проходит через массив и находит знак действия.
+
+            if(symbol[i]=='+'||symbol[i]=='-'||symbol[i]=='*'||symbol[i]=='/'){
+               symbolOperation = symbol[i]; // запоминаем значение ячейки в подготовленную переменную.
+            }
+
+            }
+        if(symbolOperation!='+'&&symbolOperation!='-'&&symbolOperation!='/'&&symbolOperation!='*'){
+                throw new IOException("Неверный формат. Используйте знаки +,-,/,* и только два операнда."); //исключение! Проверка на введение допустимого знака действия.
+        }
+
+
+
+
+        return symbolOperation;
+    }
+    private static int mathematicalCalculation(int a, int b, char op) { //метод позволяет произвести расчет введенных чисел.
+
+        switch (op) {
+            case '*':
+                return a * b;
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '/':
+                return a / b;
+            default:
+                return 0;
         }
     }
+
+
 }
